e5d1d39566f5df2c246d8c3646b3a503cf66bbd0
Aaron Kirchhoff <askirchhoff@gmail.com>
2021-08-04 16:58:41 -0700

    first commit 8/4/21

---
+++ Algorithms 2021/Arrays.js
@@ -0,0 +1,10 @@
+// add all numbers between 1 and 100 together,
+function Allnums() {
+  var tot = 0;
+  for (i = 0; i <= 100; i++) {
+      tot = tot + i;
+  }
+  return tot;
+}
+console.log(Allnums())
+// 5050
