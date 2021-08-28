240f3863c95ed70ea06ca6cca6302fefb86d1c61
Aaron Kirchhoff <askirchhoff@gmail.com>
2021-08-28 04:42:11 -0700

    highest frequency key return

--- Algorithms 2021/FrequencyCounterObjectsPractice.js
+++ Algorithms 2021/FrequencyCounterObjectsPractice.js
@@ -15,4 +15,28 @@ function squared(arr1, arr2) {
   console.log(true);
   return true
 }
-squared([2,3,4,5] , [4,9,16,25]);
+// squared([2,3,4,5] , [4,9,16,25]);
+
+
+// check to see which value shows up the most often
+
+function firstTry(Arr){
+  let max = 0;
+  let myCount = {}
+  for( let num of Arr){
+    myCount[num] = myCount[num] + 1 || 1
+  }
+  console.log(myCount);
+  for( let key in myCount){
+    if (myCount[key] > max){
+      // here I know how many times the max key shows up
+      max = myCount[key];
+      console.log(max);
+    }
+  }
+  // here i return that key
+  let ans = Object.keys(myCount).find(bat => myCount[bat] === max)
+  console.log(ans);
+  return ans;
+}
+firstTry([1,3,4,3,6,2,3,2,2,2,2,2,2]);
