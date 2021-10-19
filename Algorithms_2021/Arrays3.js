1ffc22747dc088f8d3031e7d54f09ed9b152a4cf
Aaron Kirchhoff <askirchhoff@gmail.com>
2021-10-18 18:23:13 -0700

    1221 leetcode

--- Algorithms_2021/Arrays3.js
+++ Algorithms_2021/Arrays3.js
@@ -2,4 +2,25 @@
 function removeVowels(str){
   return str.replace(/[aeiou]/g,'')
 }
-removeVowels("leetcodeisacommunityforcoders")
+// removeVowels("leetcodeisacommunityforcoders")
+
+
+// 1221. Split a String in Balanced Strings
+function balancedSubString(str){
+  var balanceVar = 0;
+  var count = 0;
+  var myArr = Array.from(str)
+  for(var i=0; i<myArr.length; i++){
+    if(myArr[i] == 'R'){
+      balanceVar--
+    }
+    if(myArr[i] == 'L'){
+      balanceVar++
+    }
+    if(balanceVar == 0){
+      count++
+    }
+  }
+  console.log(count)
+}
+balancedSubString("RLRRLLRLRL")
